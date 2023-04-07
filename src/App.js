681abfca61a510aa47952d064f7359932b6a03f2
mmaquina <mmaquina@gmail.com>
2023-04-07 00:08:49 -0300

    componentes para 2 paginas y navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar/Navbar.jsx';
+//import Home from './components/home/Home.jsx';
+import About from './components/about/About.jsx';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <Navbar/>
+      <About/>
       </header>
     </div>
   );
