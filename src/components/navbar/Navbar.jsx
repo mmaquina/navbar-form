ed3a044d8681b1ec43481158befbb8691984c780
mmaquina <mmaquina@gmail.com>
2023-04-12 02:03:28 -0300

    cambia colores de fondo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
 
   return (
     <div className="bg-opacity-0 w-screen">
-      <header className="relative inset-x-0 top-0 z-50">
+      <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="/Home" className="-m-1.5 p-1.5">
