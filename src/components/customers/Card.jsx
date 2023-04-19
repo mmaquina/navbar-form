eaf66ccdb0b836dd6c5f6338592f27f249ad619a
mmaquina <mmaquina@gmail.com>
2023-04-19 00:02:31 -0300

    mostrando las cartas

--- src/components/customers/Card.jsx
+++ src/components/customers/Card.jsx
@@ -9,12 +9,12 @@ import {
   export default function Tarjeta(props) {
     console.log(props.name)
     return (
-      <Card className="w-96">
-        <CardHeader color="blue" className="relative h-56">
+      <Card className="bg-gradient-to-bl from-violet-200 to-slate-300">
+        <CardHeader color="blue" className="relative ">
           <img
-            src="/img/blog.jpg"
+            src={props.person.image}
             alt="img-blur-shadow"
-            className="h-full w-full"
+            className=""
           />
         </CardHeader>
         <CardBody className="text-center">
@@ -22,16 +22,14 @@ import {
             {props.person.name}
           </Typography>
           <Typography>
-            The place is close to Barceloneta Beach and bus stop just 2 min by
-            walk and near to "Naviglio" where you can enjoy the main night life in
-            Barcelona.
+            {props.person.status}
           </Typography>
         </CardBody>
         <CardFooter divider className="flex items-center justify-between py-3">
-          <Typography variant="small">$899/night</Typography>
+          <Typography variant="small">{props.person.species}</Typography>
           <Typography variant="small" color="gray" className="flex gap-1">
             <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-            Barcelona, Spain
+            {props.person.gender}
           </Typography>
         </CardFooter>
       </Card>
--- src/components/customers/Customers.jsx
+++ src/components/customers/Customers.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import Tarjeta from './Card.jsx';
 
+fetch("https://rickandmortyapi.com/api/character")
+  .then((response) => response.json())  
+	.then((response) => {
+        console.log(response.results[1]);
+    });
 
-export default function Footer() {
+
+
+export default function Customers() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [customers, setCustomers] = useState(null);
+
+    useEffect(() => {
+        fetch("https://rickandmortyapi.com/api/character")
+        .then((response) => response.json())
+        .then((rj) => {
+          setCustomers(rj.results); // ⬅️ Guardar datos
+          setIsLoading(false); // ⬅️ Desactivar modo "cargando"
+        });
+    }, []);
+
+    if (isLoading) { // ⬅️ si está cargando, mostramos un texto que lo indique
+      return (
+        <div className="App">
+          <h1>Cargando...</h1>
+        </div>
+      );
+    }
+  
     return (
       <div className="container">
         <h2>Nuestros Clientes</h2>
 
-            <Tarjeta     person={{ name: 'Lin Lanying', imageId: '1bX5QH6' }} />
+        {customers.map((customer, index) => {
+          return (
+            <div key={index}>
+              <Tarjeta     person={customer} />
+            </div>
+          );
+        })}
+
       </div>
   );
 }
